8d6911bcd6b7df0b8a32de0e109278e1967f2d6b
Arcadio REYES <arcadioreyesph@gmail.com>
2022-11-11 17:02:13 +0100

    1configurating express and telling where my static folders are. 2 setting up routes

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 const express = require("express"); // loads express package
 const app = express(); // this executes an express server
 const port = 3000; // defining our server port
+
+/* configuring express and telling where my static files are located*/
+app.use(express.static("public"));
+
+/* setting up our routes*/
+
+//homepage
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/homepage.html");
+});
+//about
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+//work
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/work.html");
+});
+
+//gallery
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
